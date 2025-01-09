a166db106b41d2f613deda25a5e3320132c25078
heleno <marioanimes09@gmail.com>
2025-01-09 15:59:22 -0300

    refactor user registration and update controllers; move to users directory and add JWT verification middleware

--- src/http/routes/app-routes.ts
+++ src/http/routes/app-routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { userRoutes } from './user.routes'
+import { verifyJwt } from '../middlewares/verify-jwt.middleware'
 
 export async function appRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt)
+
   app.register(userRoutes, {
     prefix: '/users',
   })
--- src/http/routes/user.routes.ts
+++ src/http/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
-import { userRegisterController } from '../controllers/user-register.controller'
-import { userUpdateController } from '../controllers/user-update.controller'
+import { userRegisterController } from '../controllers/users/user-register.controller'
+import { userUpdateController } from '../controllers/users/user-update.controller'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/', userRegisterController)
