d637da77cd04e362cb7b405bbff289cc67285216
heleno <marioanimes09@gmail.com>
2025-01-10 16:24:45 -0300

    add userChats interface to User type; update user update route to '/edit-user'

--- src/@types/user.d.ts
+++ src/@types/user.d.ts
@@ -1,3 +1,10 @@
+interface UserChat {
+  participantId: string[]
+  lastMessage: string
+  lastTimestamp: string
+  assignedUser: 'assigned' | 'open'
+}
+
 export interface User {
   id: string
   userName: string
@@ -5,6 +12,7 @@ export interface User {
   createdAt: Date
   updatedAt: Date
   avatarUrl: string
+  userChats?: UserChat[]
 }
 
 export interface UserCreateInput {
@@ -14,6 +22,7 @@ export interface UserCreateInput {
   createdAt?: Date
   updatedAt?: Date
   avatarUrl?: string
+  userChats: UserChat[]
 }
 
 export interface UserUpdateInput extends Partial<User> {}
