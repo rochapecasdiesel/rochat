1138d9329c709fa8c096fb2c2607ad1696b26f09
Igor Heleno <igor.campos@rochapecas.com.br>
2025-01-14 10:17:25 -0300

    configure Firebase emulators with updated ports and host; add script to start emulators in package.json; create GitHub Actions workflow for E2E tests

--- vitest-environment/vitest-environment.ts
+++ vitest-environment/vitest-environment.ts
@@ -1,7 +1,6 @@
 import { Environment } from 'vitest'
 import { initializeTestEnvironment } from '@firebase/rules-unit-testing'
 import 'dotenv/config'
-import { execSync } from 'node:child_process'
 
 export default <Environment>{
   name: 'firebase',
@@ -9,14 +8,12 @@ export default <Environment>{
   async setup() {
     console.log('Inicializando ambiente Firebase para testes')
 
-    execSync('npx firebase emulators:start')
-
     // Inicializa o ambiente de teste
     const testEnv = await initializeTestEnvironment({
       projectId: process.env.PROJECT_ID, // Substitua pelo seu ID do projeto Firebase
       firestore: {
         host: 'localhost',
-        port: 8080, // Porta do emulador Firestore
+        port: 8081, // Porta do emulador Firestore
         rules: '', // Regras customizadas do Firestore (se necessário)
       },
     })
