3d345a7cb52725a10f42495da6e4acdf14e5532c
BrunoEvangelista17 <brunoevangelistag15@gmail.com>
2025-01-15 10:22:37 -0300

    Criação de Estrutura Chat

---
+++ src/@types/chat.d.ts
@@ -0,0 +1,53 @@
+export interface Alterations {
+  id: string
+  originalMessage: string
+  timestamp: Date
+}
+
+export interface Messages {
+  id: string
+  senderId: string
+  recieverId: string
+  text: string
+  timestamp: Date
+  source: 'internal' | 'external'
+  deleted: boolean
+  altered: boolean
+  alterations: Alterations[]
+}
+
+export interface MessagesCreateInput {
+  id: string
+  senderId: string
+  recieverId: string
+  text: string
+  timestamp: Date
+  source: 'internal' | 'external'
+  deleted: boolean
+  altered: boolean
+  alterations?: Alterations[]
+}
+
+export interface MessagesUpdateInput extends Partial<Messages> {}
+
+export interface Chat {
+  id: string
+  participants: string[]
+  assingnedUser: string
+  status: 'assigned' | 'open'
+  lastMessage: string
+  lastTimestamp: Date
+  messages: Messages[]
+}
+
+export interface ChatCreateInput {
+  id: string
+  participants: string[]
+  assingnedUser: string
+  status: 'assigned' | 'open'
+  lastMessage?: string
+  lastTimestamp?: Date
+  messages?: Messages[]
+}
+
+export interface ChatUpdateInput extends Partial<Chat> {}
