e8d5b64bae68e7cee0c7dc6ab52cc1750ffd0066
BrunoEvangelista17 <brunoevangelistag15@gmail.com>
2025-01-20 10:41:57 -0300

    feat: Add getMessages controller and route for retrieving chat messages

--- src/http/routes/chat.routes.ts
+++ src/http/routes/chat.routes.ts
@@ -3,10 +3,12 @@ import { createNewController } from '../controllers/chats/create-new-chat.contro
 import { createNewMessageController } from '../controllers/chats/create-new-message.controller'
 import { deleteMessageController } from '../controllers/chats/delete-message.controller'
 import { updateMessageTextController } from '../controllers/chats/update-message-text.controller'
+import { getMessagesController } from '../controllers/chats/get-messages.controller'
 
 export async function chatRoutes(app: FastifyInstance) {
   app.post('/', createNewController)
   app.post('/:chatId/message', createNewMessageController)
   app.patch('/:chatId/message/:messageId', updateMessageTextController)
   app.delete('/:chatId/message/:messageId', deleteMessageController)
+  app.get('/:chatId/messages', getMessagesController)
 }
