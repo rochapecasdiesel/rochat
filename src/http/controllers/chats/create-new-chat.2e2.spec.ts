5df21a855156bee7c8b64674626298f2f2ee259e
BrunoEvangelista17 <brunoevangelistag15@gmail.com>
2025-01-20 16:47:02 -0300

    feat: Add end-to-end tests for chat creation, message sending, deletion, and updating functionality

---
+++ src/http/controllers/chats/create-new-chat.2e2.spec.ts
@@ -0,0 +1,51 @@
+import { app } from '@/app'
+import { clearFireStore } from '@/utils/clearFireStore'
+import { generateFakeJwt } from '@/utils/generate-fake-jwt'
+
+import request from 'supertest'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Get Messages Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+    await clearFireStore()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    await clearFireStore()
+  })
+
+  it('should be able to create a chat', async () => {
+    const token1 = generateFakeJwt({ sub: '000075' })
+    const token2 = generateFakeJwt({ sub: '000076' })
+
+    await request(app.server)
+      .post('/users')
+      .set('Authorization', `Bearer ${token1}`)
+      .send({
+        userName: 'John Doe',
+        userMessage: 'test',
+        avatarUrl: '123456',
+      })
+
+    await request(app.server)
+      .post('/users')
+      .set('Authorization', `Bearer ${token2}`)
+      .send({
+        userName: 'Jane Doe',
+        userMessage: 'test',
+        avatarUrl: '123456',
+      })
+
+    const chatResponse = await request(app.server)
+      .post('/chats')
+      .set('Authorization', `Bearer ${token1}`)
+      .send({
+        participants: ['000076'],
+      })
+
+    expect(chatResponse.status).toBe(201)
+  })
+})
---
+++ src/http/controllers/chats/create-new-message.e2e.spec.ts
@@ -0,0 +1,60 @@
+import { app } from '@/app'
+import { clearFireStore } from '@/utils/clearFireStore'
+import { generateFakeJwt } from '@/utils/generate-fake-jwt'
+
+import request from 'supertest'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Create Messages Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+    await clearFireStore()
+  })
+
+  afterAll(async () => {
+    await app.close()
+    await clearFireStore()
+  })
+
+  it('should be able to messages by chatId', async () => {
+    const token1 = generateFakeJwt({ sub: '000075' })
+    const token2 = generateFakeJwt({ sub: '000076' })
+
+    await request(app.server)
+      .post('/users')
+      .set('Authorization', `Bearer ${token1}`)
+      .send({
+        userName: 'John Doe',
+        userMessage: 'test',
+        avatarUrl: '123456',
+      })
+
+    await request(app.server)
+      .post('/users')
+      .set('Authorization', `Bearer ${token2}`)
+      .send({
+        userName: 'Jane Doe',
+        userMessage: 'test',
+        avatarUrl: '123456',
+      })
+
+    const chatResponse = await request(app.server)
+      .post('/chats')
+      .set('Authorization', `Bearer ${token1}`)
+      .send({
+        participants: ['000076'],
+      })
+
+    const messageResponse1 = await request(app.server)
+      .post(`/chats/${chatResponse.body.data.chat.id}/message`)
+      .set('Authorization', `Bearer ${token1}`)
+      .send({
+        source: 'internal',
+        recieverId: '000076',
+        text: 'Hello, Jane!',
+      })
+
+    expect(messageResponse1.status).toBe(201)
+  })
+})
