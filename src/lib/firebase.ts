f8608e826a95885ff4c02e497b6fa6d47aa81471
Igor Heleno <igor.campos@rochapecas.com.br>
2025-01-21 08:57:35 -0300

    chore: Simplify Firebase configuration by removing emulator settings and using environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,48 +3,32 @@ import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { randomUUID } from 'node:crypto'
 
-const isTestEnv = env.NODE_ENV === 'test'
+export const serviceAccount = {
+  type: 'service_account',
+  project_id: env.PROJECT_ID,
+  private_key_id: env.PRIVATE_KEY_ID,
+  private_key: env.PRIVATE_KEY,
+  client_email: env.CLIENT_EMAIL,
+  client_id: env.CLIENT_ID,
+  auth_uri: env.AUTH_URI,
+  token_uri: env.TOKEN_URI,
+  auth_provider_x509_cert_url: env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: env.CLIENT_X509_CERT_URL,
+  universe_domain: env.UNIVERSE_DOMAIN,
+} as ServiceAccount
 
-// Configuração para o ambiente de teste (emulador)
-const emulatorServiceAccount = isTestEnv
-  ? {
-      type: 'service_account',
-      project_id: randomUUID(), // Identificação fictícia
-      private_key_id: 'test-key-id', // Identificação fictícia
-      private_key: 'test-key', // Não será usada, mas é necessária para inicializar
-      client_email: 'test@test-project.iam.gserviceaccount.com',
-      client_id: 'test-client-id',
-      auth_uri: 'http://localhost', // Não utilizado
-      token_uri: 'http://localhost', // Não utilizado
-      auth_provider_x509_cert_url: 'http://localhost',
-      client_x509_cert_url: 'http://localhost',
-    }
-  : {
-      type: 'service_account',
-      project_id: env.PROJECT_ID,
-      private_key_id: env.PRIVATE_KEY_ID,
-      private_key: env.PRIVATE_KEY,
-      client_email: env.CLIENT_EMAIL,
-      client_id: env.CLIENT_ID,
-      auth_uri: env.AUTH_URI,
-      token_uri: env.TOKEN_URI,
-      auth_provider_x509_cert_url: env.AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: env.CLIENT_X509_CERT_URL,
-      universe_domain: env.UNIVERSE_DOMAIN,
-    }
-
-// Inicialização do Firebase
 export const app = initializeApp({
-  credential: cert(emulatorServiceAccount as ServiceAccount),
-  projectId: isTestEnv ? 'test-project' : env.PROJECT_ID,
+  credential: cert(serviceAccount),
+  projectId: env.PROJECT_ID,
 })
 
-export const db = getFirestore(app)
+export const db = getFirestore(app, env.DATABASE)
 
-// Configuração do emulador no ambiente de teste
-if (isTestEnv) {
+// Conecte-se ao emulador no ambiente local
+if (env.NODE_ENV === 'test') {
   db.settings({
-    host: 'localhost:8081', // Porta do Firestore Emulator
+    host: 'localhost:8081', // Porta do Firestore emulador
+    projectId: randomUUID(),
     ssl: false,
   })
 }
